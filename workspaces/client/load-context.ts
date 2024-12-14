a24882602c73072545dc4ecb43105fd939791907
Sor4chi <80559385+sor4chi@users.noreply.github.com>
2024-12-14 09:58:39 +0900

    chore: setup fetch-connected infrastructure

--- workspaces/client/load-context.ts
+++ workspaces/client/load-context.ts
@@ -1,17 +1,37 @@
+import { AppType } from "@idp/server";
+import { type AppLoadContext } from "@remix-run/cloudflare";
+import { hc } from "hono/client";
 import { type PlatformProxy } from "wrangler";
-
-// When using `wrangler.toml` to configure bindings,
-// `wrangler types` will generate types for those bindings
-// into the global `Env` interface.
-// Need this empty interface so that typechecking passes
-// even if no `wrangler.toml` exists.
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
-interface Env {}
 
 type Cloudflare = Omit<PlatformProxy<Env>, "dispose">;
 
 declare module "@remix-run/cloudflare" {
   interface AppLoadContext {
     cloudflare: Cloudflare;
+    client: ReturnType<typeof hc<AppType>>;
   }
 }
+
+type GetLoadContext = (args: {
+  request: Request;
+  context: { cloudflare: Cloudflare };
+}) => AppLoadContext;
+
+export const getLoadContext: GetLoadContext = ({ context }) => {
+  const { IDP_SERVER, MODE } = context.cloudflare.env;
+
+  // https://github.com/cloudflare/workers-sdk/pull/5086
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-expect-error
+  const isProd = MODE === "production";
+  const client = hc<AppType>("http://localhost:8787", {
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-expect-error
+    fetch: isProd ? IDP_SERVER.fetch.bind(IDP_SERVER) : undefined,
+  });
+
+  return {
+    ...context,
+    client,
+  };
+};
