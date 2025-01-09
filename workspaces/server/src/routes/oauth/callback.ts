bfbb673f6aebf1f4e43b20334e68d55d95fd1c9c
Asa <26807394+a01sa01to@users.noreply.github.com>
2025-01-09 18:57:33 +0900

    fix: oauth route で許可されていないメソッドは明示的に 405 を返す

--- workspaces/server/src/routes/oauth/callback.ts
+++ workspaces/server/src/routes/oauth/callback.ts
@@ -22,126 +22,133 @@ const callbackSchema = v.object({
 	authorized: v.union([v.literal("1"), v.literal("0")]),
 });
 
-const route = app.post(
-	"/",
-	vValidator("form", callbackSchema, (res, c) => {
-		// TODO: いい感じのエラー画面を作るかも
-		if (!res.success) return c.text("Bad Request: invalid parameters", 400);
-	}),
-	async (c) => {
-		const {
-			auth_token,
-			authorized,
-			client_id,
-			redirect_uri,
-			time: _time,
-			scope,
-			state,
-		} = c.req.valid("form");
-		const time = Number.parseInt(_time, 10);
-		const nowUnixMs = Date.now();
+const route = app
+	.post(
+		"/",
+		vValidator("form", callbackSchema, (res, c) => {
+			// TODO: いい感じのエラー画面を作るかも
+			if (!res.success) return c.text("Bad Request: invalid parameters", 400);
+		}),
+		async (c) => {
+			const {
+				auth_token,
+				authorized,
+				client_id,
+				redirect_uri,
+				time: _time,
+				scope,
+				state,
+			} = c.req.valid("form");
+			const time = Number.parseInt(_time, 10);
+			const nowUnixMs = Date.now();
 
-		c.header("Cache-Control", "no-store");
-		c.header("Pragma", "no-cache");
+			c.header("Cache-Control", "no-store");
+			c.header("Pragma", "no-cache");
 
-		const publicKey = await derivePublicKey(
-			await importKey(c.env.PRIVKEY_FOR_OAUTH, "privateKey"),
-		);
-		const isValidToken = await validateAuthToken({
-			clientId: client_id,
-			redirectUri: redirect_uri,
-			scope,
-			state,
-			time,
-			key: publicKey,
-			hash: auth_token,
-		});
-		// auth_token が妥当 = client_id,redirect_uri,time,scope,state がリクエスト時と一致
-		if (!isValidToken) {
-			return c.text("Bad Request: invalid auth_token", 400);
-		}
-
-		// ログインしてることを middleware でチェック済み... な想定
-		// const userInfo = await c.var.UserRepository.fetchUserById(payload.userId);
-		const userInfo: User = {
-			id: "dummy", // memo: dev 時はここを変えよう！ Foreign key error が出るぞ！
-			initialized: true,
-			displayName: "dummy",
-			profileImageURL: "https://github.com/a01sa01to.png",
-		};
-
-		// タイムリミットは 5 min
-		if (time + 5 * 60 * 1000 < nowUnixMs) {
-			// TODO: 5 min 以内に承認してくださいみたいなメッセージ追加すべき？
-			return c.text("Bad Request: authorization request expired", 400);
-		}
-
-		const client = await c.var.OauthRepository.getClientById(client_id);
-
-		if (!client) {
-			return c.text("Internal Server Error: client not found", 500);
-		}
-
-		let redirectTo: URL;
-		if (redirect_uri) {
-			redirectTo = new URL(redirect_uri);
-		} else {
-			// `/authorize` 側で client_id に対応する callback_url は必ず存在して 1 つだけであることを保証している
-			if (!client || client.callbackUrls.length !== 1) {
-				return c.text("Internal Server Error: client callback not found", 500);
-			}
-			redirectTo = new URL(client.callbackUrls[0]);
-		}
-
-		redirectTo.searchParams.append("state", state || "");
-		if (authorized === "0") {
-			redirectTo.searchParams.append("error", "access_denied");
-			redirectTo.searchParams.append(
-				"error_description",
-				"The user denied the request",
+			const publicKey = await derivePublicKey(
+				await importKey(c.env.PRIVKEY_FOR_OAUTH, "privateKey"),
 			);
-			// redirectTo.searchParams.append('error_uri', '') // そのうち書きたいね
+			const isValidToken = await validateAuthToken({
+				clientId: client_id,
+				redirectUri: redirect_uri,
+				scope,
+				state,
+				time,
+				key: publicKey,
+				hash: auth_token,
+			});
+			// auth_token が妥当 = client_id,redirect_uri,time,scope,state がリクエスト時と一致
+			if (!isValidToken) {
+				return c.text("Bad Request: invalid auth_token", 400);
+			}
+
+			// ログインしてることを middleware でチェック済み... な想定
+			// const userInfo = await c.var.UserRepository.fetchUserById(payload.userId);
+			const userInfo: User = {
+				id: "dummy", // memo: dev 時はここを変えよう！ Foreign key error が出るぞ！
+				initialized: true,
+				displayName: "dummy",
+				profileImageURL: "https://github.com/a01sa01to.png",
+			};
+
+			// タイムリミットは 5 min
+			if (time + 5 * 60 * 1000 < nowUnixMs) {
+				// TODO: 5 min 以内に承認してくださいみたいなメッセージ追加すべき？
+				return c.text("Bad Request: authorization request expired", 400);
+			}
+
+			const client = await c.var.OauthRepository.getClientById(client_id);
+
+			if (!client) {
+				return c.text("Internal Server Error: client not found", 500);
+			}
+
+			let redirectTo: URL;
+			if (redirect_uri) {
+				redirectTo = new URL(redirect_uri);
+			} else {
+				// `/authorize` 側で client_id に対応する callback_url は必ず存在して 1 つだけであることを保証している
+				if (!client || client.callbackUrls.length !== 1) {
+					return c.text(
+						"Internal Server Error: client callback not found",
+						500,
+					);
+				}
+				redirectTo = new URL(client.callbackUrls[0]);
+			}
+
+			redirectTo.searchParams.append("state", state || "");
+			if (authorized === "0") {
+				redirectTo.searchParams.append("error", "access_denied");
+				redirectTo.searchParams.append(
+					"error_description",
+					"The user denied the request",
+				);
+				// redirectTo.searchParams.append('error_uri', '') // そのうち書きたいね
+				return c.redirect(redirectTo.href, 302);
+			}
+
+			// scope 取得
+			const requestedScopes = new Set(scope ? scope.split(" ") : []);
+			const scopes = client.scopes.filter((data) => {
+				// scope リクエストしてない場合は requestedScopes = [] なので、全部 true として付与
+				if (requestedScopes.size === 0) return true;
+				// そうでない場合はリクエストされた scope だけを付与
+				return requestedScopes.has(data.name);
+			});
+
+			// code (240bit = 8bit * 30) を生成
+			const code = binaryToBase64(crypto.getRandomValues(new Uint8Array(30)));
+
+			// access token (312bit = 8bit * 39) を生成
+			const accessToken = binaryToBase64(
+				crypto.getRandomValues(new Uint8Array(39)),
+			);
+
+			// DB に格納
+			const saveResult = await c.var.OauthRepository.createAccessToken(
+				client_id,
+				userInfo.id,
+				code,
+				redirect_uri,
+				accessToken,
+				scopes,
+			);
+
+			if (!saveResult.success) {
+				redirectTo.searchParams.append("error", "server_error");
+				redirectTo.searchParams.append("error_description", saveResult.message);
+				// redirectTo.searchParams.append('error_uri', '') // そのうち書きたいね
+				return c.redirect(redirectTo.href, 302);
+			}
+
+			redirectTo.searchParams.append("code", code);
+
 			return c.redirect(redirectTo.href, 302);
-		}
-
-		// scope 取得
-		const requestedScopes = new Set(scope ? scope.split(" ") : []);
-		const scopes = client.scopes.filter((data) => {
-			// scope リクエストしてない場合は requestedScopes = [] なので、全部 true として付与
-			if (requestedScopes.size === 0) return true;
-			// そうでない場合はリクエストされた scope だけを付与
-			return requestedScopes.has(data.name);
-		});
-
-		// code (240bit = 8bit * 30) を生成
-		const code = binaryToBase64(crypto.getRandomValues(new Uint8Array(30)));
-
-		// access token (312bit = 8bit * 39) を生成
-		const accessToken = binaryToBase64(
-			crypto.getRandomValues(new Uint8Array(39)),
-		);
-
-		// DB に格納
-		const saveResult = await c.var.OauthRepository.createAccessToken(
-			client_id,
-			userInfo.id,
-			code,
-			redirect_uri,
-			accessToken,
-			scopes,
-		);
-
-		if (!saveResult.success) {
-			redirectTo.searchParams.append("error", "server_error");
-			redirectTo.searchParams.append("error_description", saveResult.message);
-			// redirectTo.searchParams.append('error_uri', '') // そのうち書きたいね
-			return c.redirect(redirectTo.href, 302);
-		}
-
-		redirectTo.searchParams.append("code", code);
-
-		return c.redirect(redirectTo.href, 302);
-	},
-);
+		},
+	)
+	.all(async (c) => {
+		return c.text("Method Not Allowed", 405);
+	});
 
 export { route as oauthCallbackRoute };
