21cd063338d1a6be8997c0a6b989cb8b4fb9029f
Asa <26807394+a01sa01to@users.noreply.github.com>
2025-01-10 20:19:39 +0900

    feat: add database seeding scripts

--- workspaces/server/src/db/seed.sql
+++ workspaces/server/src/db/seed.sql
@@ -1,5 +1,5 @@
 -- 投入されるべきデータをここに書く
 
-INSERT OR IGNORE INTO `oauth_provider` (`id`, `name`) VALUES (1, "GitHub")
+INSERT OR IGNORE INTO `oauth_providers` (`id`, `name`) VALUES (1, "GitHub");
 
-INSERT OR IGNORE INTO `oauth_scope` (`id`, `name`, `description`) VALUES (1, "read:basic_info", "あなたのユーザー名やユーザー ID、プロフィール画像を読み取ります。")
+INSERT OR IGNORE INTO `oauth_scopes` (`id`, `name`, `description`) VALUES (1, "read:basic_info", "あなたのユーザー名やユーザー ID、プロフィール画像を読み取ります。");
