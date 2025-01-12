e878364632d1acd334d85bd25c6327fdfaef7ce0
Batrachotoxin <132897821+batora9@users.noreply.github.com>
2025-01-12 21:18:48 +0900

    vwから%に置換 (#40)

--- workspaces/client/app/components/layout/wrapper.tsx
+++ workspaces/client/app/components/layout/wrapper.tsx
@@ -6,19 +6,19 @@ export default function Wrapper() {
 		<div
 			className={css({
 				margin: "6",
-				width: "calc(100vw - token(spacing.6) * 2)",
+				width: "calc(100% - token(spacing.6) * 2)",
 				height: "calc(100dvh - token(spacing.6) * 2)",
 				overflow: "hidden",
 				background: "white",
 				borderRadius: "md",
 				lgDown: {
 					margin: "4",
-					width: "calc(100vw - token(spacing.4) * 2)",
+					width: "calc(100% - token(spacing.4) * 2)",
 					height: "calc(100dvh - token(spacing.4) * 2)",
 				},
 				mdDown: {
 					margin: "2",
-					width: "calc(100vw - token(spacing.2) * 2)",
+					width: "calc(100% - token(spacing.2) * 2)",
 					height: "calc(100dvh - token(spacing.2) * 2)",
 				},
 			})}
