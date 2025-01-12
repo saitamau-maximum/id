40c5b566f29f9f2b21c233639f46d6216e57f072
batora9 <sm.net_minecraft.hacker@outlook.jp>
2025-01-12 23:58:13 +0900

    編集UIを移植

--- workspaces/client/app/routes/settings/page.tsx
+++ workspaces/client/app/routes/settings/page.tsx
@@ -1,34 +1,45 @@
 import { css } from "styled-system/css";
+import { ProfileUpdateForm } from "./internal/components/form";
 
 export default function Settings() {
-    return (
-        <div>
-            <div
-                className={css({
-                    marginBottom: 8,
-                })}
-            >
-                <h1
-                    className={css({
-                        fontSize: "4xl",
-                        fontWeight: "bold",
-                        color: "gray.600",
-                    })}
-                >
-                    Settings
-                </h1>
-                <span className={css({ color: "gray.500", fontSize: "md" })}>
-                    プロフィール情報を変更することができます
-                </span>
-            </div>
-            <div
-                className={css({
-                    display: "flex",
-                    flexWrap: "wrap",
-                    gap: 8,
-                })}
-            >
-            </div>
-        </div>
-    );
+	return (
+		<div>
+			<div
+				className={css({
+					marginBottom: 8,
+				})}
+			>
+				<h1
+					className={css({
+						fontSize: "4xl",
+						fontWeight: "bold",
+						color: "gray.600",
+					})}
+				>
+					Settings
+				</h1>
+				<span className={css({ color: "gray.500", fontSize: "md" })}>
+					プロフィール情報を変更することができます
+				</span>
+			</div>
+			<div
+				className={css({
+					width: "100%",
+					height: "100%",
+					maxWidth: 1200,
+					margin: "auto",
+					padding: "token(spacing.8) token(spacing.4)",
+					gap: 8,
+					_after: {
+						content: "''",
+						display: "block",
+						width: "100%",
+						height: "token(spacing.8)", // スクロール時にパディング分が消えないようにするハック
+					},
+				})}
+			>
+				<ProfileUpdateForm />
+			</div>
+		</div>
+	);
 }
