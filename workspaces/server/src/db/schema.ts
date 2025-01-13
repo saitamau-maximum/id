f6a34e55f973d8fc9a378b6967aa8e709f3e28a4
batora9 <sm.net_minecraft.hacker@outlook.jp>
2025-01-14 00:07:55 +0900

    importをまとめる

--- workspaces/server/src/db/schema.ts
+++ workspaces/server/src/db/schema.ts
@@ -1,5 +1,4 @@
 import { relations } from "drizzle-orm";
-import { uniqueIndex } from "drizzle-orm/sqlite-core";
 import {
 	index,
 	int,
@@ -7,6 +6,7 @@ import {
 	primaryKey,
 	sqliteTable,
 	text,
+	uniqueIndex,
 } from "drizzle-orm/sqlite-core";
 
 export const users = sqliteTable("users", {
