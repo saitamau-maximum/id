2e6badfa775aafc62d8465e7a3a45c463fa6badc
Sor4chi <80559385+sor4chi@users.noreply.github.com>
2025-01-14 03:07:13 +0900

    chore: tmp delete migration

--- workspaces/server/drizzle/0006_seed_oauth.sql
+++
@@ -1,3 +0,0 @@
--- Custom SQL migration file, put you code below! --
-INSERT OR IGNORE INTO `oauth_providers` (`id`, `name`) VALUES (1, "GitHub");
-INSERT OR IGNORE INTO `oauth_scopes` (`id`, `name`, `description`) VALUES (1, "read:basic_info", "あなたのユーザー名やユーザー ID、プロフィール画像を読み取ります。");
--- workspaces/server/drizzle/0007_happy_lady_deathstrike.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE `user_roles` (
-	`user_id` text NOT NULL,
-	`role_id` integer NOT NULL,
-	PRIMARY KEY(`user_id`, `role_id`),
-	FOREIGN KEY (`user_id`) REFERENCES `users`(`id`) ON UPDATE no action ON DELETE no action
-);
