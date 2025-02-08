ca4e790677ed5959c5955e81f7b5e9cfe11176a9
Sor4chi <80559385+sor4chi@users.noreply.github.com>
2025-02-08 16:24:37 +0900

    feat: 更新フォームをreact-hook-form化

--- workspaces/client/app/routes/dashboard/settings/internal/components/form.tsx
+++ workspaces/client/app/routes/dashboard/settings/internal/components/form.tsx
@@ -1,14 +1,15 @@
-import { Fragment, useActionState, useCallback, useId } from "react";
+import { valibotResolver } from "@hookform/resolvers/valibot";
+import { Fragment } from "react";
+import { useForm } from "react-hook-form";
 import { css } from "styled-system/css";
+import * as v from "valibot";
 import { ButtonLike } from "~/components/ui/button-like";
 import { Form } from "~/components/ui/form";
-
-import { useNavigate } from "react-router";
-import * as v from "valibot";
+import { ErrorDisplay } from "~/components/ui/form/error-display";
 import { GRADE } from "~/constant";
 import { useAuth } from "~/hooks/use-auth";
-import { useRepository } from "~/hooks/use-repository";
 import { UserSchemas } from "~/schema/user";
+import { useUpdateProfile } from "../hooks/use-update-profile";
 
 const UpdateFormSchema = v.object({
 	displayName: UserSchemas.DisplayName,
@@ -21,134 +22,19 @@ const UpdateFormSchema = v.object({
 	grade: UserSchemas.Grade,
 });
 
-type UpdateFormSchemaType = v.InferInput<typeof UpdateFormSchema>;
-
-type UpdateFormStateType = {
-	default: Partial<UpdateFormSchemaType>;
-	error: Partial<UpdateFormSchemaType> & {
-		message?: string;
-	};
-};
+type FormValues = v.InferInput<typeof UpdateFormSchema>;
 
 export const ProfileUpdateForm = () => {
-	const { userRepository } = useRepository();
+	const { mutate, isPending } = useUpdateProfile();
 	const { user } = useAuth();
-	const displayNameId = useId();
-	const realNameId = useId();
-	const realNameKanaId = useId();
-	const displayIdId = useId();
-	const emailId = useId();
-	const academicEmailId = useId();
-	const studentId = useId();
-	const gradeId = useId();
-	const navigate = useNavigate();
 
-	const UpdateFormAction = useCallback(
-		async (
-			_prevState: UpdateFormStateType,
-			formData: FormData,
-		): Promise<UpdateFormStateType> => {
-			const displayName = formData.get("displayName");
-			const realName = formData.get("realName");
-			const realNameKana = formData.get("realNameKana");
-			const displayId = formData.get("displayId");
-			const email = formData.get("email");
-			const academicEmail = formData.get("academicEmail");
-			const studentId = formData.get("studentId");
-			const grade = formData.get("grade");
-
-			const result = v.safeParse(UpdateFormSchema, {
-				displayName,
-				realName,
-				realNameKana,
-				displayId,
-				email,
-				academicEmail,
-				studentId,
-				grade,
-			});
-
-			if (!result.success) {
-				const errors = v.flatten<typeof UpdateFormSchema>(result.issues);
-				return {
-					default: {
-						displayName: displayName?.toString() ?? undefined,
-						realName: realName?.toString() ?? undefined,
-						realNameKana: realNameKana?.toString() ?? undefined,
-						displayId: displayId?.toString() ?? undefined,
-						email: email?.toString() ?? undefined,
-						academicEmail: academicEmail?.toString() ?? undefined,
-						studentId: studentId?.toString() ?? undefined,
-						grade: grade?.toString() ?? undefined,
-					},
-					error: {
-						displayName: errors.nested?.displayName
-							? errors.nested.displayName.join(", ")
-							: undefined,
-						realName: errors.nested?.realName
-							? errors.nested.realName.join(", ")
-							: undefined,
-						realNameKana: errors.nested?.realNameKana
-							? errors.nested.realNameKana.join(", ")
-							: undefined,
-						displayId: errors.nested?.displayId
-							? errors.nested.displayId.join(", ")
-							: undefined,
-						email: errors.nested?.email
-							? errors.nested.email.join(", ")
-							: undefined,
-						academicEmail: errors.nested?.academicEmail
-							? errors.nested.academicEmail.join(", ")
-							: undefined,
-						studentId: errors.nested?.studentId
-							? errors.nested.studentId.join(", ")
-							: undefined,
-						grade: errors.nested?.grade
-							? errors.nested.grade.join(", ")
-							: undefined,
-					},
-				};
-			}
-
-			try {
-				await userRepository.update({
-					displayName: result.output.displayName,
-					realName: result.output.realName,
-					realNameKana: result.output.realNameKana,
-					displayId: result.output.displayId,
-					email: result.output.email,
-					academicEmail: result.output.academicEmail,
-					studentId: result.output.studentId,
-					grade: result.output.grade,
-				});
-
-				navigate("/");
-
-				return {
-					default: {},
-					error: {},
-				};
-			} catch (e) {
-				return {
-					default: {
-						displayName: result.output.displayName,
-						realName: result.output.realName,
-						realNameKana: result.output.realNameKana,
-						displayId: result.output.displayId,
-						email: result.output.email,
-						academicEmail: result.output.academicEmail,
-						studentId: result.output.studentId,
-						grade: result.output.grade,
-					},
-					error: { message: "ユーザー情報の更新に失敗しました" },
-				};
-			}
-		},
-		[userRepository, navigate],
-	);
-
-	const [state, submitAction, isPending] = useActionState(UpdateFormAction, {
-		default: {
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm<FormValues>({
+		resolver: valibotResolver(UpdateFormSchema),
+		defaultValues: {
 			displayName: user?.displayName,
 			realName: user?.realName,
 			realNameKana: user?.realNameKana,
@@ -158,11 +44,11 @@ export const ProfileUpdateForm = () => {
 			studentId: user?.studentId,
 			grade: user?.grade,
 		},
-		error: {},
 	});
 
 	return (
 		<form
+			onSubmit={handleSubmit((d) => mutate(d))}
 			className={css({
 				width: "100%",
 				display: "flex",
@@ -170,156 +56,67 @@ export const ProfileUpdateForm = () => {
 				gap: 6,
 				alignItems: "center",
 			})}
-			action={submitAction}
 		>
+			<Form.Field.TextInput
+				label="ID (半角英小文字、半角数字、アンダースコア(_)で3文字以上16文字以下)"
+				error={errors.displayId?.message}
+				placeholder="maximum_taro"
+				required
+				{...register("displayId")}
+			/>
+
+			<Form.Field.TextInput
+				label="ユーザー名"
+				error={errors.displayName?.message}
+				placeholder="Maximum"
+				required
+				{...register("displayName")}
+			/>
+
+			<Form.Field.TextInput
+				label="本名 (学生証に記載のもの)"
+				error={errors.realName?.message}
+				placeholder="山田 太郎"
+				required
+				{...register("realName")}
+			/>
+
+			<Form.Field.TextInput
+				label="本名 (カナ)"
+				error={errors.realNameKana?.message}
+				placeholder="ヤマダ タロウ"
+				required
+				{...register("realNameKana")}
+			/>
+
+			<Form.Field.TextInput
+				label="大学のメールアドレス"
+				error={errors.academicEmail?.message}
+				placeholder="student@ms.saitama-u.ac.jp"
+				required
+				{...register("academicEmail")}
+			/>
+
+			<Form.Field.TextInput
+				label="大学以外で連絡の取れるメールアドレス"
+				error={errors.email?.message}
+				placeholder="member@maximum.vc"
+				required
+				{...register("email")}
+			/>
+
+			<Form.Field.TextInput
+				label="学籍番号"
+				error={errors.studentId?.message}
+				placeholder="00XX000"
+				required
+				{...register("studentId")}
+			/>
+
 			<Form.FieldSet>
-				<label htmlFor={displayIdId}>
-					<Form.LabelText>
-						ID (半角英小文字、半角数字、アンダースコア(_)で3文字以上16文字以下)
-					</Form.LabelText>
-				</label>
-				<Form.Input
-					id={displayIdId}
-					placeholder="maximum_taro"
-					required
-					name="displayId"
-					defaultValue={state.default.displayId}
-				/>
-				<p
-					className={css({
-						color: "red.600",
-						fontSize: "sm",
-					})}
-				>
-					{state.error.displayId}
-				</p>
-			</Form.FieldSet>
-			<Form.FieldSet>
-				<label htmlFor={displayNameId}>
-					<Form.LabelText>ユーザー名</Form.LabelText>
-				</label>
-				<Form.Input
-					id={displayNameId}
-					placeholder="Maximum"
-					required
-					name="displayName"
-					defaultValue={state.default.displayName}
-				/>
-				<p
-					className={css({
-						color: "red.600",
-						fontSize: "sm",
-					})}
-				>
-					{state.error.displayName}
-				</p>
-			</Form.FieldSet>
-			<Form.FieldSet>
-				<label htmlFor={realNameId}>
-					<Form.LabelText>本名 (学生証に記載のもの)</Form.LabelText>
-				</label>
-				<Form.Input
-					id={realNameId}
-					placeholder="山田 太郎"
-					required
-					name="realName"
-					defaultValue={state.default.realName}
-				/>
-				<p
-					className={css({
-						color: "red.600",
-						fontSize: "sm",
-					})}
-				>
-					{state.error.realName}
-				</p>
-			</Form.FieldSet>
-			<Form.FieldSet>
-				<label htmlFor={realNameKanaId}>
-					<Form.LabelText>本名 (カナ)</Form.LabelText>
-				</label>
-				<Form.Input
-					id={realNameKanaId}
-					placeholder="ヤマダ タロウ"
-					required
-					name="realNameKana"
-					defaultValue={state.default.realNameKana}
-				/>
-				<p
-					className={css({
-						color: "red.600",
-						fontSize: "sm",
-					})}
-				>
-					{state.error.realNameKana}
-				</p>
-			</Form.FieldSet>
-			<Form.FieldSet>
-				<label htmlFor={academicEmailId}>
-					<Form.LabelText>大学のメールアドレス</Form.LabelText>
-				</label>
-				<Form.Input
-					id={academicEmailId}
-					placeholder="student@ms.saitama-u.ac.jp"
-					required
-					type="email"
-					name="academicEmail"
-					defaultValue={state.default.academicEmail}
-				/>
-				<p
-					className={css({
-						color: "red.600",
-						fontSize: "sm",
-					})}
-				>
-					{state.error.academicEmail}
-				</p>
-			</Form.FieldSet>
-			<Form.FieldSet>
-				<label htmlFor={emailId}>
-					<Form.LabelText>大学以外で連絡の取れるメールアドレス</Form.LabelText>
-				</label>
-				<Form.Input
-					id={emailId}
-					placeholder="member@maximum.vc"
-					required
-					type="email"
-					name="email"
-					defaultValue={state.default.email}
-				/>
-				<p
-					className={css({
-						color: "red.600",
-						fontSize: "sm",
-					})}
-				>
-					{state.error.email}
-				</p>
-			</Form.FieldSet>
-			<Form.FieldSet>
-				<label htmlFor={studentId}>
-					<Form.LabelText>学籍番号</Form.LabelText>
-				</label>
-				<Form.Input
-					id={studentId}
-					placeholder="00XX000"
-					required
-					name="studentId"
-					defaultValue={state.default.studentId}
-				/>
-				<p
-					className={css({
-						color: "red.600",
-						fontSize: "sm",
-					})}
-				>
-					{state.error.studentId}
-				</p>
-			</Form.FieldSet>
-			<Form.FieldSet>
-				<label htmlFor={gradeId}>
+				<legend>
 					<Form.LabelText>現在の学年</Form.LabelText>
-				</label>
+				</legend>
 				<div
 					className={css({
 						display: "grid",
@@ -328,42 +125,26 @@ export const ProfileUpdateForm = () => {
 						alignItems: "center",
 					})}
 				>
-					{GRADE.map((grade) => (
-						<Fragment key={grade.label}>
-							<Form.LabelText>{grade.label}</Form.LabelText>
+					{GRADE.map((g) => (
+						<Fragment key={g.label}>
+							<Form.LabelText>{g.label}</Form.LabelText>
 							<Form.RadioGroup>
-								{grade.identifier.map((identifier) => (
+								{g.identifier.map((identifier) => (
 									<Form.Radio
 										key={identifier}
-										name="grade"
 										value={identifier}
 										label={identifier}
-										defaultChecked={state.default.grade === identifier}
+										required
+										{...register("grade")}
 									/>
 								))}
 							</Form.RadioGroup>
 						</Fragment>
 					))}
 				</div>
-				<p
-					className={css({
-						color: "red.600",
-						fontSize: "sm",
-					})}
-				>
-					{state.error.grade}
-				</p>
+				<ErrorDisplay error={errors.grade?.message} />
 			</Form.FieldSet>
-			{state.error.message && (
-				<p
-					className={css({
-						color: "red.600",
-						fontSize: "sm",
-					})}
-				>
-					{state.error.message}
-				</p>
-			)}
+
 			<button type="submit" disabled={isPending}>
 				<ButtonLike variant="primary" disabled={isPending}>
 					更新
