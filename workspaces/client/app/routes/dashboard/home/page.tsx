ab607780000fe1fcb6a3f36cd025ef1ba1bc20a6
Sor4chi <80559385+sor4chi@users.noreply.github.com>
2025-02-20 10:42:31 +0900

    fix: Dashboard Responsive (#90)

--- workspaces/client/app/routes/dashboard/home/page.tsx
+++ workspaces/client/app/routes/dashboard/home/page.tsx
@@ -2,11 +2,13 @@ import { css } from "styled-system/css";
 import { ContributionCard } from "~/components/feature/user/contribution/card";
 import { ProfileCard } from "~/components/feature/user/profile-card";
 import { useAuth } from "~/hooks/use-auth";
+import { useDeviceType } from "~/hooks/use-device-type";
 import { useContribution } from "./internal/hooks/use-contribution";
 
 export default function Home() {
 	const { user } = useAuth();
 	const { data, isLoading } = useContribution();
+	const { deviceType } = useDeviceType();
 
 	if (!user) {
 		return null;
@@ -19,27 +21,29 @@ export default function Home() {
 				maxWidth: "1200px",
 				marginTop: 32,
 				display: "grid",
-				gridTemplateColumns: "480px max-content",
+				gridTemplateColumns: {
+					"@dashboard/4xl": "480px 360px",
+					base: "repeat(1, 1fr)",
+				},
 				justifyContent: "center",
 				placeItems: "center",
 				gap: 16,
-				lgDown: {
-					gridTemplateColumns: "repeat(1, 1fr)",
-				},
 			})}
 		>
-			<ProfileCard
-				id={user.id}
-				displayName={user.displayName}
-				realName={user.realName}
-				displayId={user.displayId}
-				profileImageURL={user.profileImageURL}
-				grade={user.grade}
-				initialized={user.initialized}
-				roles={user.roles}
-				bio={user.bio}
-			/>
-			<div>
+			<div className={css({ width: "100%", maxWidth: "480px" })}>
+				<ProfileCard
+					id={user.id}
+					displayName={user.displayName}
+					realName={user.realName}
+					displayId={user.displayId}
+					profileImageURL={user.profileImageURL}
+					grade={user.grade}
+					initialized={user.initialized}
+					roles={user.roles}
+					bio={user.bio}
+				/>
+			</div>
+			<div className={css({ width: "100%", maxWidth: "480px" })}>
 				<h1
 					className={css({
 						fontSize: "2xl",
@@ -58,18 +62,27 @@ export default function Home() {
 				>
 					Maximumでの活動度によって色がつきます
 				</p>
-				<ContributionCard
-					isLoading={isLoading}
-					weeks={
-						data?.weeks.map((week) =>
-							week.map((day) => ({
-								...day,
-								date: new Date(day.date),
-							})),
-						) || []
-					}
-					clip={14}
-				/>
+				<div
+					className={css({
+						display: "flex",
+						alignItems: "center",
+						justifyContent: "center",
+					})}
+				>
+					<ContributionCard
+						isLoading={isLoading}
+						weeks={
+							data?.weeks.map((week) =>
+								week.map((day) => ({
+									...day,
+									date: new Date(day.date),
+								})),
+							) || []
+						}
+						clip={14}
+						size={deviceType === "sp" ? "sm" : "md"}
+					/>
+				</div>
 			</div>
 		</div>
 	);
--- workspaces/client/app/routes/dashboard/members/profile/page.tsx
+++ workspaces/client/app/routes/dashboard/members/profile/page.tsx
@@ -2,6 +2,7 @@ import { useParams } from "react-router";
 import { css } from "styled-system/css";
 import { ContributionCard } from "~/components/feature/user/contribution/card";
 import { ProfileCard } from "~/components/feature/user/profile-card";
+import { useDeviceType } from "~/hooks/use-device-type";
 import { useMemberContribution } from "./internal/hooks/use-member-contribution";
 import { useMemberProfile } from "./internal/hooks/use-member-profie";
 
@@ -9,6 +10,7 @@ export default function Profile() {
 	const { userDisplayId } = useParams<{
 		userDisplayId: string;
 	}>();
+	const { deviceType } = useDeviceType();
 
 	if (!userDisplayId) {
 		return null;
@@ -50,27 +52,29 @@ export default function Profile() {
 				maxWidth: "1200px",
 				marginTop: 32,
 				display: "grid",
-				gridTemplateColumns: "480px max-content",
+				gridTemplateColumns: {
+					"@dashboard/4xl": "480px 360px",
+					base: "repeat(1, 1fr)",
+				},
 				justifyContent: "center",
 				placeItems: "center",
 				gap: 16,
-				lgDown: {
-					gridTemplateColumns: "repeat(1, 1fr)",
-				},
 			})}
 		>
-			<ProfileCard
-				id={memberProfile?.id || ""}
-				displayName={memberProfile?.displayName}
-				realName={memberProfile?.realName}
-				displayId={memberProfile?.displayId}
-				profileImageURL={memberProfile?.profileImageURL}
-				grade={memberProfile?.grade}
-				initialized={memberProfile?.initialized || false}
-				roles={memberProfile?.roles || []}
-				bio={memberProfile?.bio}
-			/>
-			<div>
+			<div className={css({ width: "100%", maxWidth: "480px" })}>
+				<ProfileCard
+					id={memberProfile?.id || ""}
+					displayName={memberProfile?.displayName}
+					realName={memberProfile?.realName}
+					displayId={memberProfile?.displayId}
+					profileImageURL={memberProfile?.profileImageURL}
+					grade={memberProfile?.grade}
+					initialized={memberProfile?.initialized || false}
+					roles={memberProfile?.roles || []}
+					bio={memberProfile?.bio}
+				/>
+			</div>
+			<div className={css({ width: "100%", maxWidth: "480px" })}>
 				<h1
 					className={css({
 						fontSize: "2xl",
@@ -89,18 +93,27 @@ export default function Profile() {
 				>
 					Maximumでの活動度によって色がつきます
 				</p>
-				<ContributionCard
-					isLoading={isMemberContributionLoading}
-					weeks={
-						memberContribution?.weeks.map((week) =>
-							week.map((day) => ({
-								...day,
-								date: new Date(day.date),
-							})),
-						) || []
-					}
-					clip={14}
-				/>
+				<div
+					className={css({
+						display: "flex",
+						alignItems: "center",
+						justifyContent: "center",
+					})}
+				>
+					<ContributionCard
+						isLoading={isMemberContributionLoading}
+						weeks={
+							memberContribution?.weeks.map((week) =>
+								week.map((day) => ({
+									...day,
+									date: new Date(day.date),
+								})),
+							) || []
+						}
+						clip={14}
+						size={deviceType === "sp" ? "sm" : "md"}
+					/>
+				</div>
 			</div>
 		</div>
 	);
