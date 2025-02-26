f3ca9e026382bd0c39da4033d29c7690b5bbe9d9
nakamuraitsuki <isukin@gmail.com>
2025-02-27 08:49:33 +0900

    fix: unifiedのパイプラインを大域で宣言

--- workspaces/client/app/hooks/use-markdown.tsx
+++ workspaces/client/app/hooks/use-markdown.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Fragment, jsx, jsxs } from "react/jsx-runtime";
 import rehypeReact from "rehype-react";
 import rehypeSanitize from "rehype-sanitize";
@@ -6,50 +6,50 @@ import remarkParse from "remark-parse";
 import remarkRehype from "remark-rehype";
 import { unified } from "unified";
 
+const markdownToReact = unified()
+	.use(remarkParse)
+	.use(remarkRehype)
+	.use(rehypeSanitize)
+	.use(rehypeReact, { jsx, jsxs, Fragment });
+
 export const useMarkdown = (markdownContent: string | undefined) => {
 	const [reactContent, setReactContent] = useState<React.ReactNode>(null);
 	const [isLoading, setIsLoading] = useState<boolean>(true);
 	const [error, setError] = useState<Error | null>(null);
 	const isMounted = useRef(true);
 
-	const parseMarkdown = useCallback(async () => {
-		if (!markdownContent) {
-			setReactContent(null);
-			setIsLoading(false);
-			return;
-		}
-
-		setIsLoading(true);
-		try {
-			const file = await unified()
-				.use(remarkParse)
-				.use(remarkRehype)
-				.use(rehypeSanitize)
-				.use(rehypeReact, { jsx, jsxs, Fragment })
-				.process(markdownContent);
-
-			if (isMounted.current) {
-				setReactContent(file.result);
-				setError(null);
-			}
-		} catch (err: unknown) {
-			setReactContent(null);
-			setError(
-				err instanceof Error ? err : new Error("Unknown error occurred"),
-			);
-		} finally {
-			setIsLoading(false);
-		}
-	}, [markdownContent]);
-
 	useEffect(() => {
 		isMounted.current = true;
+		const parseMarkdown = async () => {
+			if (!markdownContent) {
+				setReactContent(null);
+				setIsLoading(false);
+				return;
+			}
+
+			setIsLoading(true);
+			try {
+				const file = await markdownToReact.process(markdownContent);
+
+				if (isMounted.current) {
+					setReactContent(file.result);
+					setError(null);
+				}
+			} catch (err: unknown) {
+				setReactContent(null);
+				setError(
+					err instanceof Error ? err : new Error("Unknown error occurred"),
+				);
+			} finally {
+				setIsLoading(false);
+			}
+		};
 		parseMarkdown();
 
 		return () => {
 			isMounted.current = false;
 		};
-	}, [parseMarkdown]);
+	}, [markdownContent]);
 
 	return { reactContent, isLoading, error };
 };
