ee4290e6b237ebcd2e2e25b63c87268161f738c2
Sor4chi <80559385+sor4chi@users.noreply.github.com>
2025-03-03 08:10:55 +0900

    chore: deflag oauth system

--- workspaces/flag/src/index.ts
+++ workspaces/flag/src/index.ts
@@ -1,11 +1,10 @@
 const FLAGS_DEFAULT = {
 	ENABLE_LOGIN: true,
-	ENABLE_OAUTH_REGISTRATION: false,
+	ENABLE_OAUTH_REGISTRATION: true,
 };
 
 export const DEV_FLAGS = {
 	...FLAGS_DEFAULT,
-	ENABLE_OAUTH_REGISTRATION: true,
 };
 
 export const PROD_FLAGS = {
