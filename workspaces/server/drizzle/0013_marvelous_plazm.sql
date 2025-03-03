42061aae92e2723cd5bcb4be1e7f99c6cebfc98d
Asa <26807394+a01sa01to@users.noreply.github.com>
2025-03-03 21:09:57 +0900

    fix migration for #86 (#111)

---
+++ workspaces/server/drizzle/0013_marvelous_plazm.sql
@@ -0,0 +1,13 @@
+CREATE TABLE `calendar_events` (
+	`id` text PRIMARY KEY NOT NULL,
+	`user_id` text NOT NULL,
+	`title` text NOT NULL,
+	`description` text NOT NULL,
+	`start_at` text NOT NULL,
+	`end_at` text NOT NULL,
+	FOREIGN KEY (`user_id`) REFERENCES `users`(`id`) ON UPDATE no action ON DELETE no action
+);
+--> statement-breakpoint
+CREATE INDEX `user_idx` ON `calendar_events` (`user_id`);--> statement-breakpoint
+CREATE INDEX `start_at_idx` ON `calendar_events` (`start_at`);--> statement-breakpoint
+CREATE INDEX `end_at_idx` ON `calendar_events` (`end_at`);
