ca60f8faaebbd9d3796a58494e4648b42c296713
Asa <26807394+a01sa01to@users.noreply.github.com>
2025-03-14 16:48:11 +0900

    client 側 repository

--- workspaces/client/app/repository/certification.ts
+++ workspaces/client/app/repository/certification.ts
@@ -6,10 +6,27 @@ export interface CertificationRequestParams {
 	certifiedIn: number;
 }
 
+export interface CertificationRequest {
+	userId: string;
+	certificationId: string;
+	certifiedIn: number;
+}
+
+export interface CertificationRequestReviewParams {
+	userId: string;
+	certificationId: string;
+	isApproved: boolean;
+}
+
 export interface ICertificationRepository {
 	getAllCertifications(): Promise<Certification[]>;
 	getAllCertifications$$key(): unknown[];
 	requestCertification: (params: CertificationRequestParams) => Promise<void>;
+	getAllCertificationRequests: () => Promise<CertificationRequest[]>;
+	getAllCertificationRequests$$key(): unknown[];
+	reviewCertificationRequest: (
+		params: CertificationRequestReviewParams,
+	) => Promise<void>;
 }
 
 export class CertificationRepositoryImpl implements ICertificationRepository {
@@ -37,6 +54,35 @@ export class CertificationRepositoryImpl implements ICertificationRepository {
 		});
 		if (!res.ok) {
 			throw new Error("Failed to request certification");
+		}
+	}
+
+	async getAllCertificationRequests(): Promise<CertificationRequest[]> {
+		const res = await client.certification.request.$get();
+		if (!res.ok) {
+			throw new Error("Failed to fetch certification requests");
+		}
+		return await res.json();
+	}
+
+	getAllCertificationRequests$$key() {
+		return ["certificationRequests"];
+	}
+
+	async reviewCertificationRequest({
+		userId,
+		certificationId,
+		isApproved,
+	}: CertificationRequestReviewParams) {
+		const res = await client.certification.review.$put({
+			json: {
+				userId,
+				certificationId,
+				isApproved,
+			},
+		});
+		if (!res.ok) {
+			throw new Error("Failed to review certification request");
 		}
 	}
 }
