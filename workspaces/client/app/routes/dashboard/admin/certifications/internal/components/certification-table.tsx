7f5df617f9785848e8f22a10dc7443b7cf3bfb5c
Asa <26807394+a01sa01to@users.noreply.github.com>
2025-03-15 00:08:21 +0900

    refactor: use useForm

--- workspaces/client/app/routes/dashboard/admin/certifications/internal/components/certification-table.tsx
+++ workspaces/client/app/routes/dashboard/admin/certifications/internal/components/certification-table.tsx
@@ -1,11 +1,11 @@
-import { useActionState, useCallback, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { Check, Edit, Trash } from "react-feather";
+import { useForm } from "react-hook-form";
 import { css } from "styled-system/css";
 import { DeleteConfirmation } from "~/components/feature/delete-confirmation";
 import { ConfirmDialog } from "~/components/logic/callable/comfirm";
 import { Form } from "~/components/ui/form";
 import { Table } from "~/components/ui/table";
-import type { CertificationUpdateParams } from "~/repository/certification";
 import type { Certification } from "~/types/certification";
 import {
 	useDeleteCertification,
@@ -17,6 +17,53 @@ interface Props {
 }
 
 export const CertificationTable = ({ certifications }: Props) => {
+	const { mutate: deleteCertification, isPending: isPendingDeletion } =
+		useDeleteCertification();
+	const { mutate: updateCertification, isPending: isPendingUpdate } =
+		useUpdateCertification();
+	const [editing, setEditing] = useState(
+		new Array(certifications.length).fill(false),
+	);
+
+	useEffect(() => {
+		setEditing(new Array(certifications.length).fill(false));
+	}, [certifications.length]);
+
+	const { getValues, register } = useForm({
+		defaultValues: {
+			description: certifications.map((cert) => cert.description),
+		},
+	});
+
+	const handleDeleteCertification = useCallback(
+		async (certification: Certification) => {
+			const res = await ConfirmDialog.call({
+				title: "資格・試験の削除",
+				children: <DeleteConfirmation title={certification.title} />,
+				danger: true,
+			});
+			if (res.type === "dismiss") return;
+			deleteCertification(certification.id);
+		},
+		[deleteCertification],
+	);
+
+	const handleUpdateCertification = useCallback(
+		async (certification: Certification, idx: number) => {
+			const description = getValues(`description.${idx}` as const);
+			updateCertification(
+				{ certificationId: certification.id, description },
+				{
+					onSuccess: () => {
+						// index === idx なら false にして、そうでなければ val のまま
+						setEditing((arr) => arr.map((val, index) => index !== idx && val));
+					},
+				},
+			);
+		},
+		[updateCertification, getValues],
+	);
+
 	return (
 		<div>
 			<h2
@@ -37,7 +84,7 @@ export const CertificationTable = ({ certifications }: Props) => {
 						textAlign: "center",
 					})}
 				>
-					リクエストはありません
+					資格・試験はありません
 				</p>
 			) : (
 				<Table.Root>
@@ -49,129 +96,95 @@ export const CertificationTable = ({ certifications }: Props) => {
 						</Table.Tr>
 					</thead>
 					<tbody>
-						{certifications?.map((certification) => (
-							<CertificationTableRow
-								certification={certification}
-								key={certification.id}
-							/>
+						{certifications?.map((certification, idx) => (
+							<Table.Tr key={certification.id}>
+								<Table.Td>{certification.title}</Table.Td>
+								<Table.Td>
+									{editing[idx] ? (
+										<div
+											className={css({
+												display: "flex",
+												gap: 2,
+												alignItems: "center",
+											})}
+										>
+											<Form.Input
+												defaultValue={certification.description ?? ""}
+												{...register(`description.${idx}` as const)}
+											/>
+											<button
+												type="button"
+												disabled={isPendingUpdate}
+												onClick={() => {
+													handleUpdateCertification(certification, idx);
+												}}
+												className={css({
+													cursor: "pointer",
+													"&:hover": {
+														color: "green.600",
+													},
+												})}
+											>
+												<Check size={20} />
+											</button>
+										</div>
+									) : (
+										certification.description
+									)}
+								</Table.Td>
+								<Table.Td>
+									<div
+										className={css({
+											display: "flex",
+											gap: 4,
+											alignItems: "center",
+											justifyContent: "center",
+											color: editing[idx] ? "gray.300" : "inherit",
+										})}
+									>
+										<button
+											type="button"
+											disabled={isPendingDeletion || editing[idx]}
+											className={css({
+												cursor: editing[idx] ? "not-allowed" : "pointer",
+												color: editing[idx] ? "gray.300" : "inherit",
+												"&:hover": {
+													color: editing[idx] ? "gray.300" : "green.600",
+												},
+											})}
+											onClick={() => {
+												setEditing((arr) =>
+													// index == idx なら true にして、それ以外ならそのまま
+													arr.map((val, index) => val || index === idx),
+												);
+											}}
+										>
+											<Edit size={20} />
+										</button>
+										/
+										<button
+											type="button"
+											disabled={isPendingDeletion || editing[idx]}
+											className={css({
+												cursor: editing[idx] ? "not-allowed" : "pointer",
+												color: editing[idx] ? "gray.300" : "inherit",
+												"&:hover": {
+													color: editing[idx] ? "gray.300" : "rose.600",
+												},
+											})}
+											onClick={() => {
+												handleDeleteCertification(certification);
+											}}
+										>
+											<Trash size={20} />
+										</button>
+									</div>
+								</Table.Td>
+							</Table.Tr>
 						))}
 					</tbody>
 				</Table.Root>
 			)}
 		</div>
-	);
-};
-
-interface TableRowProps {
-	certification: Certification;
-}
-const CertificationTableRow = ({ certification }: TableRowProps) => {
-	const { mutate: deleteCertification, isPending: isPendingDeletion } =
-		useDeleteCertification();
-	const { mutate: updateCertification, isPending: isPendingUpdate } =
-		useUpdateCertification();
-	const [editing, setEditing] = useState(false);
-
-	const handleDeleteCertification = useCallback(async () => {
-		const res = await ConfirmDialog.call({
-			title: "資格・試験の削除",
-			children: <DeleteConfirmation title={certification.title} />,
-			danger: true,
-		});
-		if (res.type === "dismiss") return;
-		deleteCertification(certification.id);
-	}, [deleteCertification, certification]);
-
-	const [_, updateCertificationAction] = useActionState(
-		async (_: null, formData: FormData) => {
-			const description = formData.get("description");
-
-			const param: CertificationUpdateParams = {
-				certificationId: certification.id,
-				// @ts-ignore すぐなおす
-				description: description ?? "",
-			};
-
-			updateCertification(param);
-			setEditing(false);
-			return null;
-		},
-		null,
-	);
-
-	return (
-		<Table.Tr key={certification.id}>
-			<Table.Td>{certification.title}</Table.Td>
-			<Table.Td>
-				{editing ? (
-					<form
-						action={updateCertificationAction}
-						className={css({ display: "flex", gap: 2, alignItems: "center" })}
-					>
-						<Form.Input
-							name="description"
-							defaultValue={certification.description ?? ""}
-						/>
-						<button
-							type="submit"
-							disabled={isPendingUpdate}
-							className={css({
-								cursor: "pointer",
-								"&:hover": {
-									color: "green.600",
-								},
-							})}
-						>
-							<Check size={20} />
-						</button>
-					</form>
-				) : (
-					certification.description
-				)}
-			</Table.Td>
-			<Table.Td>
-				<div
-					className={css({
-						display: "flex",
-						gap: 4,
-						alignItems: "center",
-						justifyContent: "center",
-						color: editing ? "gray.300" : "inherit",
-					})}
-				>
-					<button
-						type="button"
-						disabled={isPendingDeletion || editing}
-						className={css({
-							cursor: editing ? "not-allowed" : "pointer",
-							color: editing ? "gray.300" : "inherit",
-							"&:hover": {
-								color: editing ? "gray.300" : "green.600",
-							},
-						})}
-						onClick={() => {
-							setEditing(true);
-						}}
-					>
-						<Edit size={20} />
-					</button>
-					/
-					<button
-						type="button"
-						disabled={isPendingDeletion || editing}
-						className={css({
-							cursor: editing ? "not-allowed" : "pointer",
-							color: editing ? "gray.300" : "inherit",
-							"&:hover": {
-								color: editing ? "gray.300" : "rose.600",
-							},
-						})}
-						onClick={handleDeleteCertification}
-					>
-						<Trash size={20} />
-					</button>
-				</div>
-			</Table.Td>
-		</Table.Tr>
 	);
 };
