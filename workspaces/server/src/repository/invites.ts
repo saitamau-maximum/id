f6986a107fa50f820edb502441d8a9886ff53926
batora9 <sm.net_minecraft.hacker@outlook.jp>
2025-03-26 01:36:27 +0900

    format

--- workspaces/server/src/repository/invites.ts
+++ workspaces/server/src/repository/invites.ts
@@ -7,9 +7,7 @@ export interface InviteStructure {
 }
 
 export interface IInvitesRepository {
-	createInvite: (
-		params: Omit<InviteStructure, "id">
-	) => Promise<void>;
+	createInvite: (params: Omit<InviteStructure, "id">) => Promise<void>;
 	getInviteById: (id: string) => Promise<InviteStructure>;
 	reduceInviteUsage: (id: string) => Promise<void>;
 	deleteInvite: (id: string) => Promise<void>;
