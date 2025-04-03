8cbc0666853611d84c028dbb7c23ad8670e30bd7
yukikamome316 <32415464+yukikamome316@users.noreply.github.com>
2025-04-03 16:50:17 +0900

    fix: SQLite3 で ALTER MODIFY できない問題を修正

--- workspaces/server/drizzle/0021_busy_black_bolt.sql
+++ workspaces/server/drizzle/0021_busy_black_bolt.sql
@@ -1,4 +1,13 @@
-ALTER TABLE `users` ADD `is_pending` integer NOT NULL DEFAULT 0;
-ALTER TABLE `users` MODIFY `is_pending` integer NOT NULL DEFAULT 1;--> statement-breakpoint
+ALTER TABLE users RENAME TO old_users;
 
-ALTER TABLE `users` ADD `invitation_id` text;
+CREATE TABLE `users` (
+  `id` text PRIMARY KEY NOT NULL,
+  `initialized_at` integer,
+  `is_pending` integer NOT NULL DEFAULT 1,  -- 新規ユーザーのデフォルト値は 1 にする
+  `invitation_id` text
+);
+
+-- 既存ユーザーについては is_pending = 0 とする
+INSERT INTO `users` ("id", "initialized_at", "is_pending") SELECT "id", "initialized_at", COALESCE("is_pending", 0) FROM old_users;
+
+DROP TABLE old_users;--> statement-breakpoint
