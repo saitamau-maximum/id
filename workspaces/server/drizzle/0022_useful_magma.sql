ac043d5dada1f9ee061da1520de1f8f2558ac602
yukikamome316 <32415464+yukikamome316@users.noreply.github.com>
2025-04-03 22:44:37 +0900

    fix: is_pending のマイグレーションで UPDATE を使うように修正

--- workspaces/server/drizzle/0022_useful_magma.sql
+++ workspaces/server/drizzle/0022_useful_magma.sql
@@ -1,14 +1,6 @@
-ALTER TABLE users RENAME TO old_users;
-
-CREATE TABLE `users` (
-  `id` text PRIMARY KEY NOT NULL,
-  `initialized_at` integer,
-  `is_pending` integer NOT NULL DEFAULT 1,  -- 新規ユーザーのデフォルト値は 1 にする
-  `invitation_id` text,
-  FOREIGN KEY (`invitation_id`) REFERENCES invites(id)
-);
+-- 新規・既存ユーザーに対して DEFAULT 1 とする
+ALTER TABLE `users` ADD `is_pending` integer NOT NULL DEFAULT 1;--> statement-breakpoint
+ALTER TABLE `users` ADD `invitation_id` text REFERENCES invites(id);--> statement-breakpoint
 
 -- 既存ユーザーについては is_pending = 0 とする
-INSERT INTO `users` ("id", "initialized_at", "is_pending") SELECT "id", "initialized_at", COALESCE("is_pending", 0) FROM old_users;
-
-DROP TABLE old_users;--> statement-breakpoint
+UPDATE `users` SET `is_pending` = 0;
