ec68ae43065167e9edee6e72686b5a59144b0070
Asa <26807394+a01sa01to@users.noreply.github.com>
2025-04-07 17:33:10 +0900

    refactor: write memo of merged date in flag

--- workspaces/flag/src/index.ts
+++ workspaces/flag/src/index.ts
@@ -1,7 +1,7 @@
 const FLAGS_DEFAULT = {
-	ENABLE_CERTIFICATION: true,
-	ENABLE_CALENDAR: true,
-	ENABLE_INVITE: true,
+	ENABLE_CERTIFICATION: true, // merged in 2025-03-16
+	ENABLE_CALENDAR: true, // merged in 2025-03-28
+	ENABLE_INVITE: true, // merged in 2025-04-07
 };
 
 export const DEV_FLAGS = {
