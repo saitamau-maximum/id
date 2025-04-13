40b68a4c3b4b26704a3dd870f906a2156200ae2b
Sor4chi <80559385+sor4chi@users.noreply.github.com>
2025-04-13 20:31:52 +0900

    feat(client): onboardingフォームで、外部メンバーか内部メンバーかによって学生情報の要求を切り替える

--- workspaces/client/app/routes/onboarding/internal/components/form.tsx
+++ workspaces/client/app/routes/onboarding/internal/components/form.tsx
@@ -1,12 +1,12 @@
 import { valibotResolver } from "@hookform/resolvers/valibot";
-import { Fragment } from "react";
+import { Fragment, useCallback } from "react";
 import { useForm } from "react-hook-form";
 import { css } from "styled-system/css";
 import * as v from "valibot";
 import { ButtonLike } from "~/components/ui/button-like";
 import { Form } from "~/components/ui/form";
 import { ErrorDisplay } from "~/components/ui/form/error-display";
-import { GRADE } from "~/constant";
+import { GRADE, OUTSIDE_GRADE } from "~/constant";
 import { useAuth } from "~/hooks/use-auth";
 import { UserSchemas } from "~/schema/user";
 import { useRegister } from "../hooks/use-register";
@@ -17,8 +17,8 @@ const RegisterFormSchema = v.object({
 	realNameKana: UserSchemas.RealNameKana,
 	displayId: UserSchemas.DisplayId,
 	email: UserSchemas.Email,
-	academicEmail: UserSchemas.AcademicEmail,
-	studentId: UserSchemas.StudentId,
+	academicEmail: v.optional(UserSchemas.AcademicEmail),
+	studentId: v.optional(UserSchemas.StudentId),
 	grade: UserSchemas.Grade,
 });
 
@@ -32,6 +32,8 @@ export const RegisterForm = () => {
 	const {
 		register,
 		handleSubmit,
+		watch,
+		setError,
 		formState: { errors },
 	} = useForm<FormInputValues, unknown, FormOutputValues>({
 		resolver: valibotResolver(RegisterFormSchema),
@@ -47,9 +49,31 @@ export const RegisterForm = () => {
 		},
 	});
 
+	const onSubmit = useCallback(
+		(data: FormInputValues) => {
+			const isOutsideMember = OUTSIDE_GRADE.includes(data.grade);
+			if (!isOutsideMember && !data.academicEmail) {
+				setError("academicEmail", {
+					message: "学籍番号と大学のメールアドレスは必須です",
+				});
+				return;
+			}
+			if (!isOutsideMember && !data.studentId) {
+				setError("studentId", {
+					message: "学籍番号と大学のメールアドレスは必須です",
+				});
+				return;
+			}
+			mutate(data);
+		},
+		[mutate, setError],
+	);
+
+	const isOutsideMember = OUTSIDE_GRADE.includes(watch("grade"));
+
 	return (
 		<form
-			onSubmit={handleSubmit((d) => mutate(d))}
+			onSubmit={handleSubmit(onSubmit)}
 			className={css({
 				width: "100%",
 				display: "flex",
@@ -58,72 +82,36 @@ export const RegisterForm = () => {
 				alignItems: "center",
 			})}
 		>
-			<Form.Field.TextInput
-				label="ID (半角英小文字、半角数字、アンダースコア(_)で3文字以上16文字以下)"
-				error={errors.displayId?.message}
-				placeholder="maximum_taro"
-				required
-				{...register("displayId")}
-			/>
-
-			<Form.Field.TextInput
-				label="ユーザー名"
-				error={errors.displayName?.message}
-				placeholder="Maximum"
-				required
-				{...register("displayName")}
-			/>
-
-			<Form.Field.TextInput
-				label="本名 (学生証に記載のもの)"
-				error={errors.realName?.message}
-				placeholder="山田 太郎"
-				required
-				{...register("realName")}
-			/>
-
-			<Form.Field.TextInput
-				label="本名 (カナ)"
-				error={errors.realNameKana?.message}
-				placeholder="ヤマダ タロウ"
-				required
-				{...register("realNameKana")}
-			/>
-
-			<Form.Field.TextInput
-				label="大学のメールアドレス"
-				error={errors.academicEmail?.message}
-				placeholder="student@ms.saitama-u.ac.jp"
-				required
-				{...register("academicEmail")}
-			/>
-
-			<Form.Field.TextInput
-				label="大学以外で連絡の取れるメールアドレス"
-				error={errors.email?.message}
-				placeholder="member@maximum.vc"
-				required
-				{...register("email")}
-			/>
-
-			<Form.Field.TextInput
-				label="学籍番号"
-				error={errors.studentId?.message}
-				placeholder="00XX000"
-				required
-				{...register("studentId")}
-			/>
-
 			<Form.FieldSet>
-				<legend>
-					<Form.LabelText>現在の学年</Form.LabelText>
-				</legend>
+				<h2
+					className={css({
+						display: "block",
+						fontSize: "md",
+						fontWeight: "bold",
+						color: "gray.600",
+					})}
+				>
+					はじめに、現在の学年を選択してください
+					<span className={css({ color: "rose.500", marginLeft: 1 })}>*</span>
+				</h2>
+				<p
+					className={css({
+						fontSize: "sm",
+						color: "gray.500",
+						marginBottom: 2,
+					})}
+				>
+					埼玉大学に在籍していない方で、埼玉大学を卒業した方は「卒業生」を、それ以外の方は「ゲスト」を選択してください。
+				</p>
 				<div
 					className={css({
 						display: "grid",
 						gap: "token(spacing.2) token(spacing.4)",
 						gridTemplateColumns: "auto 1fr",
 						alignItems: "center",
+						mdDown: {
+							gridTemplateColumns: "1fr !important",
+						},
 					})}
 				>
 					{GRADE.map((g) => (
@@ -145,6 +133,71 @@ export const RegisterForm = () => {
 				</div>
 				<ErrorDisplay error={errors.grade?.message} />
 			</Form.FieldSet>
+
+			<Form.Field.TextInput
+				label="ID (半角英小文字、半角数字、アンダースコア(_)で3文字以上16文字以下)"
+				error={errors.displayId?.message}
+				placeholder="maximum_taro"
+				required
+				{...register("displayId")}
+			/>
+
+			<Form.Field.TextInput
+				label="ユーザー名"
+				error={errors.displayName?.message}
+				placeholder="Maximum"
+				required
+				{...register("displayName")}
+			/>
+
+			<Form.Field.TextInput
+				label={`本名 ${isOutsideMember ? "" : "(学生証に記載のもの)"}`}
+				error={errors.realName?.message}
+				placeholder="山田 太郎"
+				required
+				{...register("realName")}
+			/>
+
+			<Form.Field.TextInput
+				label="本名 (カナ)"
+				error={errors.realNameKana?.message}
+				placeholder="ヤマダ タロウ"
+				required
+				{...register("realNameKana")}
+			/>
+
+			{!isOutsideMember && (
+				<Form.Field.TextInput
+					label="学籍番号"
+					error={errors.studentId?.message}
+					placeholder="00XX000"
+					required
+					{...register("studentId", {
+						setValueAs: (value) => (!value ? undefined : value),
+					})}
+				/>
+			)}
+
+			{!isOutsideMember && (
+				<Form.Field.TextInput
+					label="大学のメールアドレス"
+					error={errors.academicEmail?.message}
+					placeholder="student@ms.saitama-u.ac.jp"
+					required
+					type="email"
+					{...register("academicEmail", {
+						setValueAs: (value) => (!value ? undefined : value),
+					})}
+				/>
+			)}
+
+			<Form.Field.TextInput
+				label={`${isOutsideMember ? "" : "大学以外で"}連絡の取れるメールアドレス`}
+				error={errors.email?.message}
+				placeholder="member@maximum.vc"
+				required
+				{...register("email")}
+			/>
 
 			<button type="submit" disabled={isPending}>
 				<ButtonLike variant="primary" disabled={isPending}>
