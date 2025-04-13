0946befca3dd89681e7e2fafbb203336464163e2
kouta <takeuchi180121@gmail.com>
2025-04-14 05:28:59 +0900

    feat: urlが有効なものか検証

--- workspaces/client/app/utils/parse-social-link.ts
+++ workspaces/client/app/utils/parse-social-link.ts
@@ -5,7 +5,19 @@ import {
 	type SocialServiceId,
 } from "../constant";
 
+const isValidUrl = (url: string) => {
+	try {
+		new URL(url);
+		return true;
+	} catch (e) {
+		return false;
+	}
+}
+
 export const detectSocialService = (url: string) => {
+	if (!isValidUrl(url)) {
+		return SOCIAL_SERVICES_IDS.OTHER;
+	}
 	const host = new URL(url).host;
 	const socialService = SOCIAL_SERVICES_HOST_NAMES.find(
 		(service) => service.host === host,
