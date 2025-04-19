fceffbd5ce8096ce39556217bf932b58fb058061
kouta <takeuchi180121@gmail.com>
2025-04-19 20:53:42 +0900

    feat: 日付の表示をわかりやすく

--- workspaces/server/src/infrastructure/repository/discord/calendar.ts
+++ workspaces/server/src/infrastructure/repository/discord/calendar.ts
@@ -10,6 +10,21 @@ export class DiscordCalendarNotifier implements ICalendarNotifier {
 		this.webhookUrl = webhookUrl;
 	}
 
+	private formatDate(d: Date): string {
+		const days = ["日", "月", "火", "水", "木", "金", "土"];
+		const mm = (d.getMonth() + 1).toString().padStart(2, "0");
+		const dd = d.getDate().toString().padStart(2, "0");
+		const hh = d.getHours().toString().padStart(2, "0");
+		const day = days[d.getDay()];
+		return `${mm}/${dd} (${day})`;
+	}
+
+	private formatTime(d: Date): string {
+		const hh = d.getHours().toString().padStart(2, "0");
+		const mm = d.getMinutes().toString().padStart(2, "0");
+		return `${hh}:${mm}`;
+	}
+
 	async notifyEvent(event: ICalendarEventWithLocation): Promise<void> {
 		const payload = {
 			content: "**新しい予定が追加されました！**",
@@ -23,17 +38,17 @@ export class DiscordCalendarNotifier implements ICalendarNotifier {
 					fields: [
 						{
 							name: "日時",
-							value: `${event.startAt.toLocaleDateString("ja-JP", {
-								year: "numeric",
-								month: "2-digit",
-								day: "2-digit",
-							})} ${event.startAt.toLocaleTimeString("ja-JP", {
-								hour: "2-digit",
-								minute: "2-digit",
-							})} - ${event.endAt.toLocaleTimeString("ja-JP", {
-								hour: "2-digit",
-								minute: "2-digit",
-							})}`,
+							value: `${
+								this.formatDate(event.startAt) === this.formatDate(event.endAt)
+									? `${this.formatDate(event.startAt)} ${this.formatTime(event.startAt)} - ${this.formatTime(
+											event.endAt,
+										)}`
+									: `${this.formatDate(event.startAt)} ${this.formatTime(
+											event.startAt,
+										)} - ${this.formatDate(event.endAt)} ${this.formatTime(
+											event.endAt,
+										)}`
+							}`,
 						},
 						{
 							name: "活動場所",
@@ -43,7 +58,7 @@ export class DiscordCalendarNotifier implements ICalendarNotifier {
 				},
 			],
 		};
-		
+
 		try {
 			await fetch(this.webhookUrl, {
 				method: "POST",
