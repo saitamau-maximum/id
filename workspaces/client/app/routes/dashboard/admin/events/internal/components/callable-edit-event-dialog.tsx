2866cb60f052271eb26433a7edc61d77c460eed6
kouta <takeuchi180121@gmail.com>
2025-04-19 23:45:18 +0900

    fix: 型エラーをなおした

--- workspaces/client/app/routes/dashboard/admin/events/internal/components/callable-edit-event-dialog.tsx
+++ workspaces/client/app/routes/dashboard/admin/events/internal/components/callable-edit-event-dialog.tsx
@@ -9,7 +9,7 @@ import { Form } from "~/components/ui/form";
 import { ErrorDisplay } from "~/components/ui/form/error-display";
 import { useLocations } from "~/routes/dashboard/calendar/hooks/use-locations";
 import { EVENT_DESCRIPTION_MAX_LINES, EventSchemas } from "~/schema/event";
-import type { CalendarEvent } from "~/types/event";
+import type { CalendarEvent, CalendarEventWithNotify } from "~/types/event";
 import { toHTMLDateTimePickerFormat } from "~/utils/date";
 import { DescriptionFormField } from "./detail-form-field";
 
@@ -20,7 +20,7 @@ interface Props {
 type Payload =
 	| {
 			type: "success";
-			payload: CalendarEvent;
+			payload: CalendarEventWithNotify;
 	  }
 	| {
 			type: "dismiss";
@@ -65,7 +65,7 @@ export const EditEventDialog = createCallable<Props, Payload>(
 				});
 				return;
 			}
-			const updatedEvent: CalendarEvent = {
+			const updatedEvent: CalendarEventWithNotify = {
 				...event,
 				...values,
 				startAt: values.startAt,
--- workspaces/client/app/routes/dashboard/admin/events/internal/hooks/use-update-event.tsx
+++ workspaces/client/app/routes/dashboard/admin/events/internal/hooks/use-update-event.tsx
@@ -1,14 +1,14 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { useRepository } from "~/hooks/use-repository";
 import { useToast } from "~/hooks/use-toast";
-import type { CalendarEvent } from "~/types/event";
+import type { CalendarEventWithNotify } from "~/types/event";
 
 export const useUpdateEvent = () => {
 	const { calendarRepository } = useRepository();
 	const queryClient = useQueryClient();
 	const { pushToast } = useToast();
 	return useMutation({
-		mutationFn: async (event: CalendarEvent) => {
+		mutationFn: async (event: CalendarEventWithNotify) => {
 			await calendarRepository.updateEvent(event);
 			return event;
 		},
