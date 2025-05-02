2bb24d3468b2e41905f0b4203212a701c32f416f
Asa <26807394+a01sa01to@users.noreply.github.com>
2025-05-02 12:49:36 +0900

    feat: Discord Login button

--- workspaces/flag/src/index.ts
+++ workspaces/flag/src/index.ts
@@ -1,10 +1,12 @@
 const FLAGS_DEFAULT = {
 	ENABLE_CALENDAR: true, // merged in 2025-03-28
 	ENABLE_INVITE: true, // merged in 2025-04-07
+	ENABLE_DISCORD_LOGIN: false,
 };
 
 export const DEV_FLAGS = {
 	...FLAGS_DEFAULT,
+	ENABLE_DISCORD_LOGIN: true,
 };
 
 export const PROD_FLAGS = {
