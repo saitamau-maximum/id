5d2d34ae71792f5ca099a3d4889290b0bcec88a3
Asa <26807394+a01sa01to@users.noreply.github.com>
2025-06-30 19:27:45 +0900

    refactor: `DISCORD_INVITATION_URL` 削除

--- workspaces/server/src/index.ts
+++ workspaces/server/src/index.ts
@@ -25,7 +25,6 @@ import { certificationRoute } from "./routes/certification";
 import { discordRoute } from "./routes/discord";
 import { inviteRoute } from "./routes/invite";
 import { memberRoute } from "./routes/member";
-import { miscRoute } from "./routes/misc";
 import { oauthRoute } from "./routes/oauth";
 import { publicRoute } from "./routes/public";
 import { userRoute } from "./routes/user";
@@ -116,7 +115,6 @@ export const route = app
 	.route("/calendar", calendarRoute)
 	.route("/certification", certificationRoute)
 	.route("/invite", inviteRoute)
-	.route("/misc", miscRoute)
 	.route("/public", publicRoute)
 	.route("/discord", discordRoute);
 
