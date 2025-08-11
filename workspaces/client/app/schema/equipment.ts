84a4b43e1345f62aa0197fd3ad4745d3fef67aec
batora9 <sm.net_minecraft.hacker@outlook.jp>
2025-08-11 18:49:39 +0900

    fix: optional付け忘れてた

--- workspaces/client/app/schema/equipment.ts
+++ workspaces/client/app/schema/equipment.ts
@@ -6,9 +6,11 @@ export const EquipmentSchemas = {
 		v.nonEmpty("備品名を入力してください"),
 		v.maxLength(100, "備品名は100文字以下で入力してください"),
 	),
-	Description: v.pipe(
-		v.string(),
-		v.maxLength(500, "備品の説明は500文字以下で入力してください"),
+	Description: v.optional(
+		v.pipe(
+			v.string(),
+			v.maxLength(500, "備品の説明は500文字以下で入力してください"),
+		),
 	),
 	OwnerId: v.pipe(v.string(), v.nonEmpty("所有者を選択してください")),
 };
