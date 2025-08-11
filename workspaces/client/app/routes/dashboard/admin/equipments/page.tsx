6f510874f589ef5412b4c7b470ba60265e294f95
batora9 <sm.net_minecraft.hacker@outlook.jp>
2025-08-11 18:46:42 +0900

    chore: タイトル設定

--- workspaces/client/app/routes/dashboard/admin/equipments/page.tsx
+++ workspaces/client/app/routes/dashboard/admin/equipments/page.tsx
@@ -1,7 +1,11 @@
+import type { MetaFunction } from "react-router";
 import { css } from "styled-system/css";
 import { EquipmentDialog } from "./internal/components/callable-equipment-dialog";
 import { EquipmentEditor } from "./internal/components/equipments-editor";
 
+export const meta: MetaFunction = () => {
+	return [{ title: "備品管理 | Maximum IdP" }];
+};
 export default function EquipmentsPage() {
 	return (
 		<div
