cf756ad4616ef6375e887c53099e43a2f18fffef
Asa <26807394+a01sa01to@users.noreply.github.com>
2025-08-15 18:53:31 +0900

    fix: kid を generateKey 時点で付与する (#294)

--- workspaces/server/src/utils/oauth/key.ts
+++ workspaces/server/src/utils/oauth/key.ts
@@ -1,3 +1,7 @@
+import { binaryToBase64Url } from "./convert-bin-base64";
+
+type JsonWebKeyWithKid = JsonWebKey & { kid: string };
+
 const keypairGenAlgorithm = {
 	name: "ECDSA",
 	namedCurve: "P-521",
@@ -10,51 +14,84 @@ const signatureAlgorithm = {
 
 const keypairUsage = ["sign", "verify"];
 
-const generateKeyPair = () =>
-	crypto.subtle.generateKey(
-		keypairGenAlgorithm,
-		true,
-		keypairUsage,
-	) as Promise<CryptoKeyPair>;
-
-const exportKey = async (key: CryptoKey) => {
-	const exportedKey = (await crypto.subtle.exportKey("jwk", key)) as JsonWebKey;
-	return btoa(JSON.stringify(exportedKey));
-};
-
-const importKey = async (data: string, type: "publicKey" | "privateKey") => {
-	const keyData = JSON.parse(atob(data));
-	return crypto.subtle.importKey(
+export const jwkToKey = (jwk: JsonWebKey, type: "publicKey" | "privateKey") =>
+	crypto.subtle.importKey(
 		"jwk",
-		keyData,
+		jwk,
 		keypairGenAlgorithm,
 		true,
 		type === "publicKey" ? ["verify"] : ["sign"],
 	);
-};
 
-const derivePublicKey = async (privateKey: CryptoKey) => {
-	const publicKey = (await crypto.subtle.exportKey(
+export const generateKeyPair = async () => {
+	const { privateKey, publicKey } = (await crypto.subtle.generateKey(
+		keypairGenAlgorithm,
+		true,
+		keypairUsage,
+	)) as CryptoKeyPair;
+
+	const privKeyJwk = (await crypto.subtle.exportKey(
 		"jwk",
 		privateKey,
-	)) as JsonWebKey;
-	// biome-ignore lint/performance/noDelete: <explanation>
-	delete publicKey.d;
-	publicKey.key_ops = ["verify"];
-	return importKey(btoa(JSON.stringify(publicKey)), "publicKey");
+	)) as JsonWebKeyWithKid;
+	const pubKeyJwk = (await crypto.subtle.exportKey(
+		"jwk",
+		publicKey,
+	)) as JsonWebKeyWithKid;
+
+	// kid を設定
+	const kid = binaryToBase64Url(crypto.getRandomValues(new Uint8Array(16)));
+	privKeyJwk.kid = kid;
+	pubKeyJwk.kid = kid;
+
+	// use を設定 (署名用; 暗号化用にしたい場合は enc にする)
+	privKeyJwk.use = "sig";
+	pubKeyJwk.use = "sig";
+
+	return {
+		privateKey: privKeyJwk,
+		publicKey: pubKeyJwk,
+	};
 };
 
-const sign = async (payload: Uint8Array, key: CryptoKey) => {
+export const exportKey = async (key: JsonWebKeyWithKid) => {
+	return btoa(JSON.stringify(key));
+};
+
+export const importKey = async (
+	data: string,
+	type: "publicKey" | "privateKey",
+) => {
+	const { kid, ...keyData }: JsonWebKeyWithKid = JSON.parse(atob(data));
+	return {
+		jwk: {
+			...keyData,
+			kid,
+		},
+		key: await jwkToKey(keyData, type),
+	};
+};
+
+export const derivePublicKey = (
+	privateKey: JsonWebKeyWithKid,
+): JsonWebKeyWithKid => {
+	// shallow copy する
+	const publicKey = { ...privateKey };
+	// biome-ignore lint/performance/noDelete: d は秘密鍵の情報なので削除、 d = undefined とはしない
+	delete publicKey.d;
+	publicKey.key_ops = ["verify"];
+	return publicKey;
+};
+
+export const sign = async (payload: Uint8Array, key: CryptoKey) => {
 	const signedBuf = await crypto.subtle.sign(signatureAlgorithm, key, payload);
 	return new Uint8Array(signedBuf);
 };
 
-const verify = async (
+export const verify = async (
 	payload: Uint8Array,
 	key: CryptoKey,
 	signature: Uint8Array,
 ) => {
 	return crypto.subtle.verify(signatureAlgorithm, key, signature, payload);
 };
-
-export { derivePublicKey, importKey, exportKey, generateKeyPair, sign, verify };
