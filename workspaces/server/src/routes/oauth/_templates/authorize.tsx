c64d4eec894425c14e30c2222b9511e74b9e5a54
Asa <26807394+a01sa01to@users.noreply.github.com>
2025-08-25 17:34:23 +0900

    refactor: convert authorize.ts to tsx

--- workspaces/server/src/routes/oauth/_templates/authorize.tsx
+++ workspaces/server/src/routes/oauth/_templates/authorize.tsx
@@ -1,4 +1,4 @@
-import { html } from "hono/html";
+import { type FC, Fragment } from "hono/jsx";
 import { _Button } from "./button";
 
 interface AuthorizeProps {
@@ -26,152 +26,135 @@ interface AuthorizeProps {
 	};
 }
 
-export const _Authorize = ({
+export const _Authorize: FC<AuthorizeProps> = ({
 	appName,
 	appLogo,
 	scopes,
 	oauthFields,
 	user,
-}: AuthorizeProps) => html`
-  <div class="max-w-md md:space-y-8 space-y-4">
-    <div>
-      <div class="flex items-center justify-center gap-4 md:mb-2">
-        ${
-					appLogo
-						? html`<img
-              src="${appLogo}"
-              alt="${appName} のロゴ"
-              width="64"
-              height="64"
-              class="rounded-full object-cover border-[1px] border-gray-200 md:w-[64px] md:h-[64px] w-[48px] h-[48px]"
-            />`
-						: ""
-				}
-        <h1 class="text-3xl font-bold text-center">${appName}</h1>
-      </div>
-      <span class="block text-lg font-normal text-gray-600 text-center">
-        を承認しますか？
-      </span>
-    </div>
-    <div class="md:space-y-6 space-y-4">
-      <p class="text-md text-gray-800 text-center">
-        承認すると ${appName} は以下の情報にアクセスできるようになります。
-      </p>
-      <div class="bg-gray-50 md:p-4 p-2 rounded-lg">
-        <table class="border-collapse table-auto w-full text-sm">
-          <tbody>
-            ${scopes.map(
-							(data) => html`
-                <tr class="[&:not(:last-child)]:border-b-[1px] border-gray-200">
-                  <td class="px-4 py-2 font-medium">${data.name}</td>
-                  <td class="px-4 py-2 font-normal text-gray-500">
-                    ${data.description}
-                  </td>
-                </tr>
-              `,
-						)}
-          </tbody>
-        </table>
-      </div>
-      <form method="POST" action="/oauth/callback" class="space-y-4">
-        <input type="hidden" name="client_id" value="${oauthFields.clientId}" />
-        <input type="hidden" name="response_type" value="${oauthFields.responseType}" />
-        ${
-					oauthFields.responseMode
-						? html`<input
-              type="hidden"
-              name="response_mode"
-              value="${oauthFields.responseMode}"
-            />`
-						: ""
-				}
-        ${
-					oauthFields.redirectUri
-						? html`<input
-              type="hidden"
-              name="redirect_uri"
-              value="${oauthFields.redirectUri}"
-            />`
-						: ""
-				}
-        ${
-					oauthFields.scope
-						? html`<input
-              type="hidden"
-              name="scope"
-              value="${oauthFields.scope}"
-            />`
-						: ""
-				}
-        ${
-					oauthFields.state
-						? html`<input
-              type="hidden"
-              name="state"
-              value="${oauthFields.state}"
-            />`
-						: ""
-				}
-        ${
-					oauthFields.oidcNonce
-						? html`<input
-              type="hidden"
-              name="oidc_nonce"
-              value="${oauthFields.oidcNonce}"
-              />`
-						: ""
-				}
-        ${
-					oauthFields.oidcAuthTime
-						? html`<input
-              type="hidden"
-              name="oidc_auth_time"
-              value="${oauthFields.oidcAuthTime.toString()}"
-            />`
-						: ""
-				}
-        <input type="hidden" name="time" value="${oauthFields.nowUnixMs}" />
-        <input type="hidden" name="auth_token" value="${oauthFields.token}" />
-        <div class="flex justify-around gap-4">
-          ${_Button({
-						text: "承認する",
-						variant: "primary",
-						type: "submit",
-						name: "authorized",
-						value: "1",
-					})}
-          ${_Button({
-						text: "拒否する",
-						variant: "secondary",
-						type: "submit",
-						name: "authorized",
-						value: "0",
-					})}
-        </div>
-      </form>
-    </div>
-    <p class="text-sm text-gray-600 text-center">
-      ${new URL(oauthFields.redirectTo).origin} へリダイレクトします。
-      このアドレスが意図しているものか確認してください。
-    </p>
-  </div>
-  <div
-    class="flex items-center justify-center flex-wrap gap-2 absolute top-4 left-0 right-0"
-  >
-    ${
-			user.profileImageUrl
-				? html`<img
-          src="${user.profileImageUrl}"
-          alt="${user.displayName} のアイコン"
-          width="32"
-          height="32"
-          class="rounded-full object-cover border-[1px] border-gray-200"
-        />`
-				: ""
-		}
-    <span class="md:text-lg text-gray-600 font-bold">${user.displayName}</span>
-    <span class="md:text-sm text-xs text-gray-600"
-      >さんとしてログインしています。</span
-    >
-  </div>
-`;
+}) => (
+	<Fragment>
+		<div className="max-w-md md:space-y-8 space-y-4">
+			<div>
+				<div className="flex items-center justify-center gap-4 md:mb-2">
+					{appLogo && (
+						<img
+							src={appLogo}
+							alt={`${appName} のロゴ`}
+							width="64"
+							height="64"
+							className="rounded-full object-cover border-[1px] border-gray-200 md:w-[64px] md:h-[64px] w-[48px] h-[48px]"
+						/>
+					)}
+					<h1 className="text-3xl font-bold text-center">{appName}</h1>
+				</div>
+				<span className="block text-lg font-normal text-gray-600 text-center">
+					を承認しますか？
+				</span>
+			</div>
+			<div className="md:space-y-6 space-y-4">
+				<p className="text-md text-gray-800 text-center">
+					承認すると {appName} は以下の情報にアクセスできるようになります。
+				</p>
+				<div className="bg-gray-50 md:p-4 p-2 rounded-lg">
+					<table className="border-collapse table-auto w-full text-sm">
+						<tbody>
+							{scopes.map((data) => (
+								<tr
+									key={data.name}
+									className="[&:not(:last-child)]:border-b-[1px] border-gray-200"
+								>
+									<td className="px-4 py-2 font-medium">{data.name}</td>
+									<td className="px-4 py-2 font-normal text-gray-500">
+										{data.description}
+									</td>
+								</tr>
+							))}
+						</tbody>
+					</table>
+				</div>
+				<form method="post" action="/oauth/callback" className="space-y-4">
+					<input type="hidden" name="client_id" value={oauthFields.clientId} />
+					<input
+						type="hidden"
+						name="response_type"
+						value={oauthFields.responseType}
+					/>
+					{oauthFields.responseMode && (
+						<input
+							type="hidden"
+							name="response_mode"
+							value={oauthFields.responseMode}
+						/>
+					)}
+					{oauthFields.redirectUri && (
+						<input
+							type="hidden"
+							name="redirect_uri"
+							value={oauthFields.redirectUri}
+						/>
+					)}
+					{oauthFields.scope && (
+						<input type="hidden" name="scope" value={oauthFields.scope} />
+					)}
+					{oauthFields.state && (
+						<input type="hidden" name="state" value={oauthFields.state} />
+					)}
+					{oauthFields.oidcNonce && (
+						<input
+							type="hidden"
+							name="oidc_nonce"
+							value={oauthFields.oidcNonce}
+						/>
+					)}
+					{oauthFields.oidcAuthTime && (
+						<input
+							type="hidden"
+							name="oidc_auth_time"
+							value={oauthFields.oidcAuthTime.toString()}
+						/>
+					)}
+					<input type="hidden" name="time" value={oauthFields.nowUnixMs} />
+					<input type="hidden" name="auth_token" value={oauthFields.token} />
+					<div className="flex justify-around gap-4">
+						{_Button({
+							text: "承認する",
+							variant: "primary",
+							type: "submit",
+							name: "authorized",
+							value: "1",
+						})}
+						{_Button({
+							text: "拒否する",
+							variant: "secondary",
+							type: "submit",
+							name: "authorized",
+							value: "0",
+						})}
+					</div>
+				</form>
+			</div>
+			<p className="text-sm text-gray-600 text-center">
+				{new URL(oauthFields.redirectTo).origin} へリダイレクトします。
+				このアドレスが意図しているものか確認してください。
+			</p>
+		</div>
+		<div className="flex items-center justify-center flex-wrap gap-2 absolute top-4 left-0 right-0">
+			{user.profileImageUrl && (
+				<img
+					src={user.profileImageUrl}
+					alt={`${user.displayName} のアイコン`}
+					width="32"
+					height="32"
+					className="rounded-full object-cover border-[1px] border-gray-200"
+				/>
+			)}
+			<span className="md:text-lg text-gray-600 font-bold">
+				{user.displayName}
+			</span>
+			<span className="md:text-sm text-xs text-gray-600">
+				さんとしてログインしています。
+			</span>
+		</div>
+	</Fragment>
+);
