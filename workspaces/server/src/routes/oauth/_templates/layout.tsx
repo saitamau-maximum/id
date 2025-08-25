36607b642944b937db14245d78e6536a7c0dce1d
Asa <26807394+a01sa01to@users.noreply.github.com>
2025-08-25 17:39:29 +0900

    refactor: convert layout.ts to tsx

--- workspaces/server/src/routes/oauth/_templates/layout.tsx
+++ workspaces/server/src/routes/oauth/_templates/layout.tsx
@@ -1,4 +1,4 @@
-import { html } from "hono/html";
+import type { FC } from "hono/jsx";
 import type { HtmlEscapedString } from "hono/utils/html";
 
 interface LayoutProps {
@@ -6,30 +6,25 @@ interface LayoutProps {
 	subtitle?: string;
 }
 
-const TITLE = "Maximum Auth";
+const TITLE = "Maximum IdP OAuth";
 const ORG_NAME = "埼玉大学 プログラミングサークル Maximum";
 
-export const _Layout = ({ subtitle, children }: LayoutProps) => html`
-  <!doctype html>
-  <html lang="ja">
-    <head>
-      <meta charset="UTF-8" />
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <meta name="robots" content="noindex" />
-      <title>${subtitle ? `${subtitle} | ${TITLE}` : TITLE}</title>
-      <script src="https://cdn.tailwindcss.com"></script>
-    </head>
-    <body>
-      <main
-        class="flex flex-col items-center justify-center p-4 w-screen h-screen"
-      >
-        ${children}
-      </main>
-      <footer
-        class="text-sm text-gray-500 absolute bottom-2 w-full text-center"
-      >
-        &copy; ${new Date().getFullYear()} ${ORG_NAME}
-      </footer>
-    </body>
-  </html>
-`;
+export const _Layout: FC<LayoutProps> = ({ subtitle, children }) => (
+	<html lang="ja">
+		<head>
+			<meta charset="UTF-8" />
+			<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+			<meta name="robots" content="noindex" />
+			<title>{subtitle ? `${subtitle} | ${TITLE}` : TITLE}</title>
+			<script src="https://cdn.tailwindcss.com" />
+		</head>
+		<body>
+			<main className="flex flex-col items-center justify-center p-4 w-screen h-screen">
+				{children}
+			</main>
+			<footer className="text-sm text-gray-500 absolute bottom-2 w-full text-center">
+				&copy; {new Date().getFullYear()} {ORG_NAME}
+			</footer>
+		</body>
+	</html>
+);
