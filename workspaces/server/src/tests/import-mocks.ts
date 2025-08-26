cc7d581e7376a0764d4f96db04ab661fda9e6649
Asa <26807394+a01sa01to@users.noreply.github.com>
2025-08-26 22:12:41 +0900

    fix: use `importMock`

--- workspaces/server/src/tests/import-mocks.ts
+++ workspaces/server/src/tests/import-mocks.ts
@@ -2,6 +2,6 @@ import { vi } from "vitest";
 
 // Worker で import できないが、どうせ使わないので mock
 // ref: https://developers.cloudflare.com/workers/testing/vitest-integration/known-issues/#importing-modules-from-global-setup-file
-vi.mock("discord-api-types/v10", () => ({ default: vi.fn() }));
-vi.mock("ics", () => ({ default: vi.fn() }));
-vi.mock("wasm-image-optimization", () => ({ default: vi.fn() }));
+vi.importMock("discord-api-types/v10");
+vi.importMock("ics");
+vi.importMock("wasm-image-optimization");
