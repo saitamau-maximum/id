7a332c41059c6ad5261df01193112bea6b9c2c08
Asa <26807394+a01sa01to@users.noreply.github.com>
2025-08-26 11:37:17 +0900

    test: add oauth2 spec test (WIP)

---
+++ workspaces/server/src/routes/oauth/oauth2.test.ts
@@ -0,0 +1,10 @@
+// OAuth 2.0 の仕様に沿っているかのテスト
+// ref: https://datatracker.ietf.org/doc/html/rfc6749
+
+import { describe, expect, it } from "vitest";
+
+describe("OAuth2 spec", () => {
+	it("temporary", () => {
+		expect(1).toBe(1);
+	});
+});
