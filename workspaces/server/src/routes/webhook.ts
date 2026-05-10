e804623334eaea39e78eeda67cf2e57d2891416c
Asa <26807394+a01sa01to@users.noreply.github.com>
2026-05-10 15:31:41 +0900

    feat: add GitHub webhook endpoint

--- workspaces/server/src/routes/webhook.ts
+++ workspaces/server/src/routes/webhook.ts
@@ -1,10 +1,26 @@
+import { Webhooks } from "@octokit/webhooks";
 import { factory } from "../factory";
 
 const app = factory.createApp();
 
-const route = app.post("/github", (c) => {
-	// TODO
-	return c.text("OK", 200);
+const route = app.post("/github", async (c) => {
+	// ここでしか使わないので、特に抽象化せず @octokit/webhooks を使う
+	const webhooks = new Webhooks({
+		secret: c.env.GITHUB_WEBHOOK_SECRET,
+	});
+
+	webhooks.on("ping", () => {
+		console.log("GitHub Webhook Received: ping -> pong");
+	});
+
+	await webhooks.verifyAndReceive({
+		id: c.req.header("x-github-delivery") ?? "",
+		name: c.req.header("x-github-event") ?? "",
+		signature: c.req.header("x-hub-signature-256") ?? "",
+		payload: await c.req.text(),
+	});
+
+	return c.body(null, 204);
 });
 
 export { route as webhookRoute };
