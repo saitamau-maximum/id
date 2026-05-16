5c72a103fde6c213c7cfd3f3d41e01df6aa740ff
Asato Tsuchiya <26807394+a01sa01to@users.noreply.github.com>
2026-05-16 14:05:13 +0900

    feat(OIDC): `id_token` に `nickname` と `preferred_username` を追加する (#377)

--- workspaces/server/src/routes/oauth/callback.ts
+++ workspaces/server/src/routes/oauth/callback.ts
@@ -166,6 +166,8 @@ const route = app
 						...(scopes.find((s) => s.id === SCOPE_IDS.PROFILE)
 							? {
 									name: userInfo.realName,
+									nickname: userInfo.displayName,
+									preferred_username: userInfo.displayId,
 									picture: userInfo.profileImageURL,
 								}
 							: {}),
